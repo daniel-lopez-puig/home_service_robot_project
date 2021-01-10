84dff92a720fd6ff7b9687b9b1340ad1bd69613a
Daniel Lopez Puig <daniel.lopez.puig@gmail.com>
2021-01-10 08:53:54 -0800

    feat: test_slam script

---
+++ home_service_robot/scripts/test_slam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+. ../../../../devel/setup.bash
+xterm  -e  " source /opt/ros/kinetic/setup.bash; cd ${DIR}; source ../../../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=`rospack find home_service_robot`/worlds/daniel.world " &
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; cd ${DIR}; source ../../../../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; cd ${DIR}; source ../../../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; cd ${DIR}; source ../../../../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch " & 
